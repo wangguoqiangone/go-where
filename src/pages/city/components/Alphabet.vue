8a05b3f6a4a62e043a9c3ec89daa70b883e62c4f
wangguoqiang123 <1014188115@qq.com>
2018-08-13 22:52:45 +0800

    city-list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+	<ul class="list">
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+		<li class="item">A</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+	.list
+		display: flex
+		width: .4rem
+		flex-direction: column
+		justify-content: center
+		position: absolute
+		top: 1.58rem
+		right: 0
+		bottom: 0
+		.item
+			text-align: center
+			line-height: .44rem
+			color: $bgColor		
+</style>
